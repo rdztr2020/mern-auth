1c3cbfcf499d929e2d6b6d49686ef4cca9e76bbf
windowslee <slee12463@gmail.com>
2024-08-28 14:58:47 -0400

    page without database

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <div className='animate-spin rounded-full border-t-4 h-10 w-10'>
+            🤓
+            </div>
+      </div>
+    );
+  }
+
+ 

ab5e0e12ba9e2096d620c89a35ecfa19c4b60abe
windowslee <slee12463@gmail.com>
2024-08-28 22:25:36 -0400

    add mongodb and mongoose connect

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,44 +1,44 @@
-import User from "../models/user.model"
-import {connect} from '../mongodb/mongoose'
+import User from '../models/user.model';
 
-export const createOrUpdateUser =  async (
-    id,
-    first_name,
-    last_name,
-    image_url,
-    email_addresses,
-    username
+import { connect } from '../mongodb/mongoose';
+
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username
 ) => {
-    try{
+  try {
     await connect();
 
-    const user = User.findOneAndUpdate(
-        {clerkId:id},
-        {
-            $set:{
-                firstName:first_name,
-                lastName: last_name,
-                avatar: image_url,
-                email: email_addresses[0].email,
-                username:username
-            },
+    const user = await User.findOneAndUpdate(
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          avatar: image_url,
+          email: email_addresses[0].email,
+          username: username,
         },
-        {new:true,upsert:true}
+      },
+      { new: true, upsert: true }
     );
-    return user;
-}
 
-catch(error){
-    console.log('Error creating or updating user:', error)
-}
+    return user;
+  } catch (error) {
+    console.log('Error creating or updating user:', error);
+  }
 };
 
 export const deleteUser = async (id) => {
-    try{
-       await connect(); 
-       await User.findOneAndDelete({clerkId:id})
-    }
-    catch (error){
-        console.log('Error deleting user:',error)
-    }
-}
+  try {
+    await connect();
+
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log('Error deleting user:', error);
+  }
+};
--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,6 +31,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
